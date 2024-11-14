6e578ad24bdce6f26829dafe2c69808abc5b29bc
SCNULMH <hjs8396@naver.com>
2024-11-14 20:04:04 +0900

    필요없는index.html 삭제

--- src/main/java/com/example/test/controller/WebController.java
+++ src/main/java/com/example/test/controller/WebController.java
@@ -6,10 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
-    @GetMapping("/index")
-    public String index() {
-        return "index";  // index.html 페이지를 반환합니다.
-    }
 
     @GetMapping("/")
     public String mainPage() {
